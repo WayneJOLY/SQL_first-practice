be5b717e9581f0e6a775d92e762116a77015d279
Gerald <geraldjoly191@gmail.com>
2024-08-16 00:34:51 -0300

    Junté tablas con queries

--- Vehicles Database.sql
+++ Vehicles Database.sql
@@ -1,5 +1,5 @@
 create table vehicles(
-	id serial primary key,
+	vehicle_id serial primary key,
 	Model varchar(100),
 	Wheels int,
 	Doors int,
@@ -13,6 +13,7 @@ insert into vehicles(Model,Wheels,Doors,VhehicleType) values
 ('McLaren Formula 1',4,0,'Race'),
 ('Tesla S',4,4,'Sedan');
 
+drop table vehicles;
 
 select *from vehicles;
 
@@ -20,14 +21,15 @@ select Model,VhehicleType from vehicles;
 
 
 create table users(
-	id serial primary key,
+	user_id serial primary key,
 	firstname varchar(100),
 	lastname  varchar(100),
 	email varchar(100) unique,
 	phone varchar(100)
 );
 
-select *from users
+select *from users;
+drop table users;
 
 insert into users(firstname,lastname,email,phone) values
 ('Gerald ','JOLY','geraldjoly@gmail.com','146523'),
@@ -37,8 +39,24 @@ insert into users(firstname,lastname,email,phone) values
 ('Jeanne ','BAZLAIT','jeannebazlait@gmail.com','126723'),
 ('Franck ','BAZLAIT','franckbazlait@gmail.com','126333');
 
+create table users_cars(
+	users_cars_id serial primary key,
+	user_id int references users(user_id),
+	car_id int references vehicles(vehicle_id)
+);
 
-create table users_vehicles(
-	id serial primary key,
+select *from users_cars;
+
+insert into users_cars (user_id , car_id) values
+
+	(1,4),
+	(1,2),
+	(3,1)
+;
+
+
+select *from users inner join users_cars
+	on users.user_id = users_cars.user_id
 	
-)
+	--inner join vehicles
+	--on  users_cars.car_id = vehicles.car_id 
